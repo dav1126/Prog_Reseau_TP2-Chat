e74df2858036c5c038ec17683c424d28c4f14fdf
0345162 <0345162@LQ2108-10.cegeplimoilou.lan>
2016-01-14 14:07:04 -0500

    commit

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -1,10 +1,16 @@
 package Model;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class Model
 {
+	Socket socket;
+	
 	public static void main(String[] args)
 	{
 		
@@ -12,8 +18,41 @@ public class Model
 	
 	public void openClientSocket(String remoteIpAddress, int remotePort)
 	{
-		InetAddress adresse = InetAddress.getByName(ipAddress);
+		InetAddress adress = null;
+		try
+		{
+			adress = InetAddress.getByName(remoteIpAddress);
+		} 
+		catch (UnknownHostException e)
+		{
+			e.printStackTrace();
+		}
 		
-		
+		socket = null;	
+		try
+		{
+			socket =  new Socket(adress, remotePort);
+		} 
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public void sendMessage(String message)
+	{
+		if (socket != null)
+		{
+			try
+			{
+				OutputStream output = socket.getOutputStream();
+				output.write(new String("Mon premier message client").getBytes());	
+			} 
+			catch (IOException e)
+			{
+				
+				System.out.println("Output failure");
+			}
+		}		
 	}
 }
