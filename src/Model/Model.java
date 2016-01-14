ba4b4c91a327b66f0c12c21581d652db5a1b79c3
0345162 <0345162@LQ2108-10.cegeplimoilou.lan>
2016-01-14 13:54:27 -0500

    first commit

---
+++ src/Model/Model.java
@@ -0,0 +1,19 @@
+package Model;
+
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class Model
+{
+	public static void main(String[] args)
+	{
+		
+	}
+	
+	public void openClientSocket(String remoteIpAddress, int remotePort)
+	{
+		InetAddress adresse = InetAddress.getByName(ipAddress);
+		
+		
+	}
+}
